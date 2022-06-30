36c64f39eab25b25455a5a7889e12214a9af1bd0
neel.k <neel.k@zymr.com>
2022-06-30 11:53:15 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hal loda hal, Bhadwa Ghar bhego tha.</h1>
+        <h1>Okay Done.</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
